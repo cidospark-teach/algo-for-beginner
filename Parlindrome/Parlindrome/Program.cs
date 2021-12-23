fd733008c1f8227233975ff5466e61bf3d907e35
cidospark-teach <engrcidos@outlook.com>
2021-12-23 11:01:13 +0100

    added parlindrom to this branch

---
+++ Parlindrome/Parlindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Helper;
+
+namespace Parlindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parlindrome is a word or phrase that reads the same backward or forward
+
+            Console.WriteLine("CONFIRM IF THE WORD A PARLINDROME!\n");
+            Challenge.DetermineParlindrome("tattarrattat");
+        }
+    }
+}
