8e5862f9170b6585ebd63b992a59aaa05de4fb3d
cidospark-teach <93078472+cidospark-teach@users.noreply.github.com>
2021-12-24 13:46:27 +0100

    Update Program.cs

--- BinarySearchGeneric/MainProj/Program.cs
+++ BinarySearchGeneric/MainProj/Program.cs
@@ -8,14 +8,10 @@ namespace MainProj
         static void Main(string[] args)
         {
             var arr = new int[] { 1, 2, 3, 4, 5, 6 };
-<<<<<<< HEAD
-            var res = BinarySearchChallenge.Search<int>(arr, 10) <= 0? "Not found!" : "Found!";
-=======
             int searchTerm = 5;
 
             var res = !BinarySearchChallenge.Search<int>(arr, searchTerm) ?
                 "Not found!" : $"Found! {searchTerm} at index {Array.IndexOf(arr, searchTerm)}";
->>>>>>> binary-search
              
             Console.WriteLine(res);
         }
