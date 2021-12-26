e8950b2a56efd0fff7d5f0f9206d529fec2a7402
cidospark-teach <engrcidos@outlook.com>
2021-12-26 09:21:19 +0100

    add numric item to result list

--- ReversePolishNotation/SolutionLib/Challenge.cs
+++ ReversePolishNotation/SolutionLib/Challenge.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SolutionLib
 {
-    public class Class1
+    public class Challenge
     {
+        public static double Evaluate(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return 0.0;
+
+            var splittedStr = str.Split(" ");
+
+            var result = new List<double>();
+
+            foreach(var item in splittedStr)
+            {
+                double number;
+
+                var isNumeric = double.TryParse(item, out number);
+                if (isNumeric)
+                {
+                    result.Add(number);
+                }
+                
+            }
+        }
     }
 }
