a60b39f1bea0e3453cc6ebe624e3be08fdc68b0c
Cidospark <cidospark27@gmail.com>
2022-01-24 00:26:10 +0100

    hashing algo implemented

---
+++ SecurityHashWithSalt/MainProj/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MainProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var challenge = new ClassLib.Challenge();
+
+            var hashResult = challenge.GenHash("123");
+            var salt = hashResult.Salt;
+            var hash = hashResult.HashValue;
+
+            Console.Write($"Salt: ");
+            foreach (var item in salt)
+                Console.Write($"{item}");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine($"Hash: ");
+            foreach (var item in hash)
+                Console.Write($"{item}");
+        }
+    }
+}
---
+++ SecurityHashWithSalt/SecurityHashWithSalt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecurityHashWithSalt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
